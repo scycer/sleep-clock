994853491dd532155d0e6ad76db8fae87617a89c
scycer <scycer1@hotmail.com>
2024-04-04 08:36:18 +1100

    SW added\

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,18 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.ts')
+      .then(registration => {
+        console.log('SW registered:', registration)
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed:', registrationError)
+      })
+  })
+}
